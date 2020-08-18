dce9c61d4da226e8b4e0825c69fd7d10fa6e7520
Kyu <kykim102@gmail.com>
2020-08-18 16:00:22 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("listening on " + PORT);
+});
+
 app.get('/', function(req, res){
   res.send("Hello world!");
 })
-
-app.listen(3000);
