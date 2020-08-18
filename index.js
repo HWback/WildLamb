89175ce2b34ec376b6dbf19ce2486c72fa62cc25
Kyu <kykim102@gmail.com>
2020-08-18 17:24:57 +1200

    Add mocha chai and update files

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("listening on " + PORT);
+app.listen(8080, function () {
+  console.log("listening on 8080");
 });
 
 app.get('/', function(req, res){
-  res.send("Hello World!!");
+  res.send('Hello World!!');
 })
